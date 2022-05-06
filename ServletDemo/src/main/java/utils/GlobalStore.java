3a01a609df5f754d0dac0f62cf9fcb4639a8a575
RyanOxley11 <103455698+RyanOxley11@users.noreply.github.com>
2022-05-06 14:32:54 -0400

    Add files via upload

---
+++ ServletDemo/src/main/java/utils/GlobalStore.java
@@ -0,0 +1,22 @@
+package utils;
+/*
+public class GlobalStore {
+    private static DataObject obj;
+    private static DataObject superObject;
+
+    public static DataObject getObj() {
+        return obj;
+    }
+
+    public static void setObj(DataObject obj) {
+        GlobalStore.obj = obj;
+    }
+
+    public static DataObject getSuperObject() {
+        return superObject;
+    }
+
+    public static void setSuperObject(DataObject superObject) {
+        GlobalStore.superObject = superObject;
+    }
+}*/
